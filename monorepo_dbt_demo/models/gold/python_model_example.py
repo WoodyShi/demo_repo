7d5f47ad3d143a2f51fdc870fd816b2e262afc13
Woody Shi <shiwoodyqc@gmail.com>
2026-04-15 16:55:22 +0800

    format py files

--- monorepo_dbt_demo/models/gold/python_model_example.py
+++ monorepo_dbt_demo/models/gold/python_model_example.py
@@ -3,11 +3,10 @@ from python_helpers.helper import helper_function
 
 
 def model(dbt, session):
-    dbt.config(materialized = "incremental")
+    dbt.config(materialized="incremental")
     df = dbt.ref("Portal__product_component")
 
     if dbt.is_incremental:
-
         # only new rows compared to max in current table
         max_from_this = f"select max(updated_at) from {dbt.this}"
         df = df.filter(df.updated_at >= session.sql(max_from_this).collect()[0][0])
@@ -15,10 +14,9 @@ def model(dbt, session):
         # or only rows from the past 3 days
         df = df.filter(df.updated_at >= F.date_add(F.current_timestamp(), F.lit(-3)))
 
-    '''
+    """
     other transformation code here
-    '''
+    """
     df = df.withColumn("helper_used", F.lit(helper_function()))
-        
 
     return df
